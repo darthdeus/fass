9dad61520ac5257f7514dba0f3b86f3534ed095c
Jakub Arnold <darthdeus@gmail.com>
2014-05-15 10:20:47 +0200

    Add basic pretty printer

---
+++ Fass/Printer.hs
@@ -0,0 +1,10 @@
+module Fass.Printer where
+
+import Fass.Types
+
+prettyPrint = concatMap prettyPrintEntity
+
+prettyPrintEntity :: Entity -> String
+prettyPrintEntity (Variable k v) = '$' : k ++ ":" ++ v ++ ";\n"
+prettyPrintEntity (Rule k v) = k ++ ": " ++ v ++ ";\n"
+prettyPrintEntity (Ruleset s inner) = s ++ " {\n" ++ prettyPrint inner ++ "}\n"
--- main.hs
+++ main.hs
@@ -3,6 +3,9 @@ module Main where
 import Fass.Parser
 import Fass.Compiler
 import Fass.Types
+import Fass.Printer
+
+import Data.Either
 
 main = do
     text <- readFile "sample.scss"
