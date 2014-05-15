48258c3604e9078f9d8ae82550c753edcdb536d8
Jakub Arnold <darthdeus@gmail.com>
2014-05-15 09:39:56 +0200

    Extract property parsers

--- main.hs
+++ main.hs
@@ -30,6 +30,7 @@ parseRule = do
     many space
     char ':'
     many space
+    -- TODO - make this more strict in terms of what can a value be
     value <- many1 $ letter <|> char '$' <|> char '#'
     optional $ char ';'
     return $ Rule property value
@@ -42,13 +43,24 @@ parseSelector = do
 parseVariable :: Parser Entity
 parseVariable = do
     char '$'
-    name <- many1 $ letter <|> char '-'
+    name <- parsePropertyName
     many $ char ' '
     char ':'
     many $ char ' '
-    value <- many1 $ char '#' <|> letter <|> char '-'
+    value <- parsePropertyValue
     optional $ char ';'
     return $ Variable name value
+
+parsePropertyName :: Parser Property
+parsePropertyName = many1 $ letter <|> char '-'
+
+parsePropertyValue :: Parser Value
+parsePropertyValue = many1 $ letter <|> char '-' <|> char '#'
+    -- Not sure how to solve this prefix, since the return type in the
+    -- monad isn't a string, so I can't concatenate it with the value
+    -- itself
+    -- prefix <- optional (char '#')
+
 
 main = do
     text <- readFile "sample.scss"
