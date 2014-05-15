b446802d63f7a3b5df281d414a4d1cfa5282dfe4
Jakub Arnold <darthdeus@gmail.com>
2014-05-15 10:14:40 +0200

    Basic tree flatten, not yet fully functional

--- Fass/Compiler.hs
+++ Fass/Compiler.hs
@@ -1,18 +1,28 @@
 module Fass.Compiler
   ( flatten
-  , flattenEntity)
+  , expandSelector)
   where
 
 import Fass.Types
 
 flatten :: [Entity] -> [Entity]
-flatten = flattenWithPrefix ""
+flatten xs = concatMap unwrap $ expandSelectorWithPrefix "" xs
 
-flattenWithPrefix :: String -> [Entity] -> [Entity]
-flattenWithPrefix prefix = map (flattenEntity prefix)
+expandSelectorWithPrefix :: String -> [Entity] -> [Entity]
+expandSelectorWithPrefix prefix = map (expandSelector prefix)
 
-flattenEntity :: String -> Entity -> Entity
-flattenEntity prefix v@(Variable _ _) = v
-flattenEntity prefix r@(Rule _ _) = r
-flattenEntity prefix (Ruleset s inner) = Ruleset newPrefix (flattenWithPrefix newPrefix inner)
-              where newPrefix = s ++ " " ++ prefix
+expandSelector :: String -> Entity -> Entity
+expandSelector prefix v@(Variable _ _) = v
+expandSelector prefix r@(Rule _ _) = r
+expandSelector prefix (Ruleset s inner) = Ruleset newPrefix (expandSelectorWithPrefix newPrefix inner)
+                 where newPrefix = s ++ " " ++ prefix
+
+unwrap :: Entity -> [Entity]
+unwrap v@(Variable _ _) = [v]
+unwrap r@(Rule _ _)     = [r]
+unwrap r@(Ruleset _ inner) = r : filter isRuleset inner
+
+isRuleset :: Entity -> Bool
+isRuleset (Ruleset _ _) = True
+isRuleset _             = False
+
--- main.hs
+++ main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Fass.Parser
+import Fass.Compiler
+import Fass.Types
 
 main = do
     text <- readFile "sample.scss"
