5faf231729693483dd79b2f1eec2bf51cf497b98
Jakub Arnold <darthdeus@gmail.com>
2014-06-01 14:23:49 +0200

    Fix newtype wrappers

--- Fass/Types.hs
+++ Fass/Types.hs
@@ -8,12 +8,24 @@ type Selector = String
 type Property = String
 type Value = String
 
-data Entity = Ruleset Selector [Entity]
-            | Variable String String
-            | Rule Property Value
-            deriving (Show)
+-- data Entity = Ruleset Selector [Entity]
+--             | Variable String String
+--             | Rule Property Value
+--             deriving (Show)
 
-newtype CSSDocument = CSSDocument [CSSEntity]
+-- newtype CSSDocument = CSSDocument [CSSEntity]
 
-data CSSEntity = CSSImport String
-               | CSSRuleset String
+-- data CSSEntity = CSSImport String
+--                | CSSRuleset String
+
+
+data SASSRuleset = SASSRuleset Selector [SASSEntity]
+                   deriving (Eq, Show)
+
+data SASSEntity = SASSVariable String String
+                | SASSRule String String
+                | SASSNestedRuleset SASSRuleset
+                | SASSNothing
+                  deriving (Eq, Show)
+
+type SASSEnv = M.Map String String
