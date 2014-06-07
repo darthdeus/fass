10dd4951d77b60906cbe3d3788a4443af42130cd
Jakub Arnold <darthdeus@gmail.com>
2014-06-07 10:29:45 +0200

    Add some actual tests

--- test/Main.hs
+++ test/Main.hs
@@ -4,16 +4,25 @@ import Test.Hspec
 import Text.Parsec.Error
 import Text.Parsec.Pos
 import Fass
+import Fass.Types
 
 instance Eq ParseError where
-    x == y = errorPos x == errorPos y &&
-             errorMessages x == errorMessages y
+  x == y = errorPos x == errorPos y &&
+   errorMessages x == errorMessages y
 
 foo :: Either Int String
 foo = Left 3
 
 main :: IO ()
-main = hspec $
+main = hspec $ do
   describe "parser" $ do
-    it "handles invalid data" $
-      parseSCSS "invalid string" `shouldBe` Left (newErrorMessage (UnExpect "s") (newPos "SCSS Parser" 1 9))
+    it "handles invalid data" $ do
+      let (Left err) = parseSCSS "invalid string"
+      (sourceLine . errorPos $ err) `shouldBe` 1
+      (sourceColumn . errorPos $ err) `shouldBe` 9
+
+    it "parses simple CSS" $
+      parseSCSS "p { color: #fff; }" `shouldBe` Right [SASSNestedRuleset (SASSRuleset "p" [SASSRule "color" "#fff"])]
+
+    it "parses variables" $
+      parseSCSS "$a: 3" `shouldBe` Right [SASSVariable "a" "3"]
