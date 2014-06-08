9d1af510f8317af6492cdb71cbd7135e28c1a386
Jakub Arnold <darthdeus@gmail.com>
2014-06-08 16:32:51 +0200

    Add basic evaluator spec

---
+++ test/Fass/EvaluatorSpec.hs
@@ -0,0 +1,15 @@
+module Fass.EvaluatorSpec where
+
+import Control.Monad.State
+import Fass.Types
+import Fass.Evaluator
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "inlineVariable" $ do
+        it "does nothing for an empty ruleset" $ do
+            let input = SASSRuleset "p" []
+            flip evalState emptyEnv (inlineVariables input) `shouldBe` input
+
+    it "foo" (True `shouldBe` True)
