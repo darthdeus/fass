d221da5534a3249029d504415d28368d1a3c035d
Jakub Arnold <darthdeus@gmail.com>
2014-06-15 13:52:04 +0200

    Add monoid instance for Fass.Selector

--- src/Fass/Types.hs
+++ src/Fass/Types.hs
@@ -1,17 +1,39 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Fass.Types where
 
+import Control.Lens
+import Data.Monoid
+import Data.String
 import qualified Data.Map as M
 
--- type Document = [Entity]
-
-type Selector = String
 type Property = String
 type Value = String
 
 -- data Entity = Ruleset Selector [Entity]
 --             | Variable String String
 --             | Rule Property Value
+newtype Selector = Selector String deriving (Show, Eq)
+
+instance Monoid Selector where
+    mempty = Selector ""
+    mappend (Selector x) (Selector y) = Selector (x ++ " " ++ y)
+
+instance IsString Selector where
+    fromString x = Selector x
+
+data Ruleset = Ruleset Selector [Entity] deriving (Eq, Show)
+data Entity = Variable String String
+              | Rule String String
+              | Nested Ruleset
+              | Null
+                deriving (Eq, Show)
+
+type SASSEnv = M.Map String String
+
+makePrisms ''Ruleset
+makePrisms ''Entity
+
+
 --             deriving (Show)
 
 -- newtype CSSDocument = CSSDocument [CSSEntity]
