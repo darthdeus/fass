f0993a978f1b272ba3b6a4b0e081e9d08e8f7fe3
Jakub Arnold <darthdeus@gmail.com>
2014-06-15 18:56:47 +0200

    Inline variables on entities before flattening the structure

--- src/Fass/Compiler.hs
+++ src/Fass/Compiler.hs
@@ -1,5 +1,6 @@
 module Fass.Compiler where
 
+import Control.Monad.State
 import Control.Lens
 import Fass.Parser
 import Fass.Evaluator
@@ -9,8 +10,16 @@ import Fass.Types
 compile :: String -> String
 compile input = case parseSCSS input of
     Left err -> fail $ show err
-    Right result -> compileEverything $ result ^.. traverse._Nested
+    Right result -> compileEverything result
 
-compileEverything :: [Ruleset] -> String
+compileEverything :: [Entity] -> String
 compileEverything [] = ""
-compileEverything entities = prettyPrint $ concatMap flatten $ inlineList entities
+compileEverything entities =
+    prettyPrint $ concatMap flatten $ rulesets
+
+    where
+      inlined :: [Entity]
+      inlined = flip evalState emptyEnv $ mapM inlineEntity entities
+
+      rulesets :: [Ruleset]
+      rulesets = inlined ^.. traverse._Nested
