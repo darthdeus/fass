609ad5ab8bd6a2492e9e81f83b942fb295edba52
George <somu@so.mu>
2014-06-15 23:42:05 +0400

    Implement .sass syntax

--- src/Fass/Compiler.hs
+++ src/Fass/Compiler.hs
@@ -2,24 +2,32 @@ module Fass.Compiler where
 
 import Control.Monad.State
 import Control.Lens
-import Fass.Parser
 import Fass.Evaluator
 import Fass.Printer
 import Fass.Types
 
-compile :: String -> String
-compile input = case parseSCSS input of
-    Left err -> fail $ show err
-    Right result -> compileEverything result
 
-compileEverything :: [Entity] -> String
+import Fass.Parsers.Sass (parseSass)
+import Fass.Parsers.SCSS (parseSCSS)
+
+import Text.Parsec (ParseError)
+import Data.List.Split
+import Control.Lens
+import Fass.Evaluator
+import Fass.Printer
+import Fass.Types
+
+compile :: Either ParseError [Entity] -> String
+compile entities = case entities of Left err  -> fail $ show err
+                                    Right res -> compileEverything $ res ^.. traverse._Nested
+
+compileEverything :: [Ruleset] -> String
 compileEverything [] = ""
-compileEverything entities =
-    prettyPrint $ concatMap flatten $ rulesets
+compileEverything entities = prettyPrint $ concatMap flatten entities
 
-    where
-      inlined :: [Entity]
-      inlined = flip evalState emptyEnv $ mapM inlineEntity entities
-
-      rulesets :: [Ruleset]
-      rulesets = inlined ^.. traverse._Nested
+compileFile :: String -> IO String
+compileFile f = do
+  code <- readFile f
+  let parse = case last $ splitOn "." f of "sass" -> parseSass
+                                           _      -> parseSCSS
+  return $ compile (parse code)
---
+++ src/Fass/Parsers/SCSS.hs
@@ -0,0 +1,56 @@
+module Fass.Parsers.SCSS where
+
+import Control.Applicative ((*>), (<*))
+import Control.Monad
+import Fass.Types
+
+import qualified Data.Text as T
+import Text.Parsec
+import Text.Parsec.String
+
+entity :: Parser Entity
+entity = do
+    value <- try variable <|> try rule <|> ruleset'
+    void spaces
+    return value
+
+paddedChar :: Char -> Parser ()
+paddedChar c = void $ spaces >> char c >> spaces
+
+ruleset' :: Parser Entity
+ruleset' = do
+    s <- selector'
+    entities <- paddedChar '{' *> many entity <* paddedChar '}'
+    return $ Nested (Ruleset s entities)
+
+rule :: Parser Entity
+rule = do
+    void spaces
+    property <- propertyName
+
+    value <- paddedChar ':' *> propertyValue
+
+    optional $ char ';'
+    return $ Rule property value
+
+selector' :: Parser Selector
+selector' = do
+    result <- many1 $ letter <|> oneOf " .#-_:>[]=" <|> digit
+    return . Selector . T.unpack . T.strip . T.pack $ result
+
+variable :: Parser Entity
+variable = do
+    name <- char '$' *> propertyName
+    paddedChar ':'
+    value <- propertyValue
+    optional $ char ';'
+    return $ Variable name value
+
+propertyName :: Parser Property
+propertyName = many1 $ letter <|> oneOf "_-*"
+
+propertyValue :: Parser Value
+propertyValue = many1 $ noneOf ";"
+
+parseSCSS :: String -> Either ParseError [Entity]
+parseSCSS = parse (many entity <* eof) "SCSS Parser"
