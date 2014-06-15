08d463c20bc22e5f9697cf6219a85924938eaede
George <somu@so.mu>
2014-06-16 01:11:50 +0400

    Include Compiler changes from previous commits

--- src/Fass/Compiler.hs
+++ src/Fass/Compiler.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Fass.Compiler where
 
+import Control.Monad.State
+import Control.Lens
+
 import qualified Data.Text as T
 import Text.Parsec (ParseError)
-import Control.Lens
 
 import Fass.Parsers.Sass (parseSass)
 import Fass.Parsers.SCSS (parseSCSS)
@@ -14,11 +16,15 @@ import Fass.Types
 
 compile :: Either ParseError [Entity] -> String
 compile entities = case entities of Left err  -> fail $ show err
-                                    Right res -> compileEverything $ res ^.. traverse._Nested
+                                    Right res -> compileEverything res
 
-compileEverything :: [Ruleset] -> String
+compileEverything :: [Entity] -> String
 compileEverything [] = ""
-compileEverything entities = prettyPrint $ concatMap flatten entities
+compileEverything entities = prettyPrint $ concatMap flatten rulesets
+  where inlined :: [Entity]
+        inlined = flip evalState emptyEnv $ mapM inlineEntity entities
+        rulesets :: [Ruleset]
+        rulesets = inlined ^.. traverse._Nested
 
 compileFile :: String -> IO String
 compileFile f = do
