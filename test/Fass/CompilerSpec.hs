f06f64568dcd0afd2856ff6c484d31c824f7767f
Jakub Arnold <darthdeus@gmail.com>
2014-06-15 18:25:15 +0200

    Add basic test runner for sass-spec

--- test/Fass/CompilerSpec.hs
+++ test/Fass/CompilerSpec.hs
@@ -1,6 +1,9 @@
 module Fass.CompilerSpec where
 
+import Data.List
 import Fass.Compiler
+import System.Directory
+import System.FilePath.Posix
 import Test.Hspec
 
 spec :: Spec
@@ -10,3 +13,19 @@ spec = describe "Compiler" $ do
         let output = "p span {\ncolor: red;\n}\n\n"
 
         compile input `shouldBe` output
+
+    it "passes the sass-spec tests" $ do
+        root <- getCurrentDirectory
+        let prefix = root </> "sass-spec" </> "spec" </> "basic"
+        folders <- getDirectoryContents $ prefix
+
+        let tests = folders \\ [".", ".."]
+
+        mapM_ (\x -> runSpec $ prefix </> x) tests
+
+runSpec :: FilePath -> IO ()
+runSpec prefix = do
+    input <- readFile $ prefix </> "input.scss"
+    expectedOutput <- readFile $ prefix </> "expected_output.css"
+
+    compile input `shouldBe` expectedOutput
