05053f40ea4f75ba4ea1f73ff1938a2a8a937395
Jakub Arnold <darthdeus@gmail.com>
2014-06-15 02:51:53 +0200

    Remove dead code

--- src/Fass/Evaluator.hs
+++ src/Fass/Evaluator.hs
@@ -14,27 +14,12 @@ inlineVariables :: SASSRuleset -> State SASSEnv SASSRuleset
 inlineVariables (SASSRuleset s []) = return $ SASSRuleset s []
 inlineVariables (SASSRuleset s entities) = mapM inlineEntity entities >>= return . SASSRuleset s
 
-
 inlineEntity :: SASSEntity -> State SASSEnv SASSEntity
 inlineEntity x = case x of
             SASSVariable name value -> modify (M.insert name value) >> return SASSNothing
             SASSRule name value -> SASSRule name <$> expandValue value
             SASSNestedRuleset _ -> return SASSNothing
             _ -> return SASSNothing
-
--- compile :: SASSRuleset -> State SASSEnv SASSRuleset
--- compile (SASSRuleset selector entities) = liftM (SASSRuleset selector) $ compileEntities entities
-
--- compileEntities :: [SASSEntity] -> State SASSEnv [SASSEntity]
--- compileEntities xs = filter (/= SASSNothing) <$> forM xs compileEntity
-
--- compileEntity :: SASSEntity -> State SASSEnv SASSEntity
--- compileEntity SASSNothing = return SASSNothing
--- compileEntity (SASSVariable name value) = modify (M.insert name value) >> return SASSNothing
--- compileEntity (SASSRule name value) = SASSRule name <$> expandValue value
--- compileEntity (SASSNestedRuleset (SASSRuleset selector rules)) = do
---     c <- compileEntities rules
---     return . SASSNestedRuleset $ SASSRuleset selector c
 
 expandValue :: String -> State SASSEnv String
 expandValue value = if isVariableName value
@@ -44,9 +29,3 @@ expandValue value = if isVariableName value
 isVariableName :: String -> Bool
 isVariableName ('$':_) = True
 isVariableName _ = False
-
--- test :: [SASSEntity]
--- test = flip evalState emptyEnv $ compileEntities exampleData
-
--- exampleData :: [SASSEntity]
--- exampleData = [SASSVariable "light-text" "#fafafa", SASSVariable "dark-text" "#0f0f0", SASSRule "color" "$light-text"]
