e62937db4bbf311304c272f74190aa5f3fd98fb8
Jakub Arnold <darthdeus@gmail.com>
2014-07-06 20:31:51 +0200

    Add basic specs for hexColorShort parser

---
+++ test/Fass/Parser/ColorSpec.hs
@@ -0,0 +1,22 @@
+module Fass.Parser.ColorSpec where
+
+import Fass.Parser.Color
+import Test.Hspec
+import Text.Parsec
+import Text.Parsec.String
+
+testParser :: Parser a -> String -> Either ParseError a
+testParser parser = parse parser "test parser"
+
+matchRight :: (Show a, Show b, Eq b) => Either a b -> b -> IO ()
+matchRight ex y = case ex of
+    Left x -> fail $ show x
+    Right x -> x `shouldBe` y
+
+
+spec :: Spec
+spec = do
+    describe "hexColorShort" $ do
+        it "works for a simple hex color" $ do
+            testParser hexColorShort "#fff" `matchRight` "fff"
+            testParser hexColorShort "#123" `matchRight` "123"
