3e0f2360bc1de545de22470548837a14627bba0d
Jakub Arnold <darthdeus@gmail.com>
2014-07-06 23:35:01 +0200

    Remove Hex type from Arithmetic

--- src/Fass/Arithmetic.hs
+++ src/Fass/Arithmetic.hs
@@ -2,11 +2,8 @@
 module Fass.Arithmetic where
 
 import Data.Monoid
-import Data.List.Split
-import Numeric
 
-data Value = Hex Int
-           | RGBA Int Int Int Float -- ^ rgb can be represented as rgba(_,_,_,1.0)
+data Value = RGBA Int Int Int Float -- ^ rgb can be represented as rgba(_,_,_,1.0)
            | Number Int             -- ^ and later optimized away when compiling
            | Literal String
            deriving Show
@@ -15,7 +12,6 @@ data Value = Hex Int
 -- debugging purposes, but rather for compiling the value down for concatenation
 -- which can't be done in any other way, such as "#abc + hello".
 stringRep :: Value -> String
-stringRep (Hex x) = show x -- TODO - convert this to actual hex int
 stringRep (Literal s) = s
 stringRep (Number x) = show x
 stringRep (RGBA r g b a) = if a == 1.0
@@ -27,12 +23,11 @@ stringRep (RGBA r g b a) = if a == 1.0
 instance Monoid Value where
     mempty = Literal ""
 
-    mappend (Hex x) (Hex y) = Hex (x + y)
     mappend (Number x) (Number y) = Number (x + y)
     mappend (RGBA r1 g1 b1 a1) (RGBA r2 g2 b2 a2) =
         RGBA (r1 + r2) (g1 + g2) (b1 + b2) (a1 + a2)
+    mappend (Literal x) (Literal y) = Literal $ x ++ y
     mappend x y = Literal $ stringRep x ++ stringRep y
-                -- TODO - handle the missing cases
 
 data Expr = Lit Value
           | OpPlus Expr Expr
