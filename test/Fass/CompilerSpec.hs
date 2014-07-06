78703d3223eeb0355d9d1213b092234c9c0c1da9
Jakub Arnold <darthdeus@gmail.com>
2014-07-07 00:49:15 +0200

    Add specs for import resolution

--- test/Fass/CompilerSpec.hs
+++ test/Fass/CompilerSpec.hs
@@ -26,23 +26,8 @@ spec = describe "Compiler" $ do
         result <- compile input
         result `shouldBe` "a {\n  color: red; }\n"
 
-    describe "inlineImportWithFile" $ do
-        it "is a noop for non-import entities" $ do
-            let input = Rule "foo" "bar"
-            inlineImportWithFile input >>= shouldBe [input]
-
-        it "impots a given file" $ do
-            let tmp = "/tmp/fass-test.scss"
-            withFile tmp WriteMode (\h -> hPutStrLn h "p { color: red; }")
-
-            result <- inlineImportWithFile (Import tmp)
-
-            -- in case the test fails, remove teh file first
-            removeFile tmp
-
-            result `shouldBe` [Nested (Ruleset "p" [Rule "color" "red"])]
-
-        it "resolves imports recursively" $ do
+    describe "compileFile" $ do
+        it "looks up imports in the file's directory" $ do
             let tmp1 = "/tmp/fass-test1.scss"
             let tmp2 = "/tmp/fass-test2.scss"
 
@@ -52,7 +37,47 @@ spec = describe "Compiler" $ do
             withFile tmp2 WriteMode
                 (\h -> hPutStrLn h "p { color: red; }")
 
-            result <- deepResolve [Import tmp1]
+            result <- compileFile tmp1
+
+            removeFile tmp1
+            removeFile tmp2
+
+            minify result `shouldBe` "p {color: red;}"
+
+
+    describe "inlineImportWithFile" $ do
+        it "is a noop for non-import entities" $ do
+            cwd <- getCurrentDirectory
+
+            let input = Rule "foo" "bar"
+            inlineImportWithFile cwd input >>= shouldBe [input]
+
+        it "impots a given file" $ do
+            cwd <- getCurrentDirectory
+
+            let tmp = "/tmp/fass-test.scss"
+            withFile tmp WriteMode (\h -> hPutStrLn h "p { color: red; }")
+
+            result <- inlineImportWithFile cwd (Import tmp)
+
+            -- in case the test fails, remove teh file first
+            removeFile tmp
+
+            result `shouldBe` [Nested (Ruleset "p" [Rule "color" "red"])]
+
+        it "resolves imports recursively" $ do
+            cwd <- getCurrentDirectory
+
+            let tmp1 = "/tmp/fass-test1.scss"
+            let tmp2 = "/tmp/fass-test2.scss"
+
+            withFile tmp1 WriteMode
+                (\h -> hPutStrLn h "@import \"/tmp/fass-test2.scss\";")
+
+            withFile tmp2 WriteMode
+                (\h -> hPutStrLn h "p { color: red; }")
+
+            result <- deepResolve cwd [Import tmp1]
 
             removeFile tmp1
             removeFile tmp2
@@ -61,7 +86,6 @@ spec = describe "Compiler" $ do
 
 runSpec :: FilePath -> IO ()
 runSpec prefix = do
-    input <- readFile $ prefix </> "input.scss"
     expectedOutput <- readFile $ prefix </> "expected_output.css"
 
     putStrLn prefix
@@ -71,7 +95,7 @@ runSpec prefix = do
     -- but there is one when there is more than one ruleset. I'm not 100%
     -- sure about this, which is why I'll be trimming them here in both cases.
 
-    left <- fmap trim (compile input)
+    left <- fmap trim $ compileFile $ prefix </> "input.scss"
     let right = (trim expectedOutput)
 
     -- putStrLn $ left ++ "\n\n"
