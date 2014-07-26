3ebe71ab876f4ba52fea64588ba4bc9e164d68e8
Jakub Arnold <darthdeus@gmail.com>
2014-07-26 12:43:02 +0200

    Add some more TODOs

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Fass.Compiler
 
 main :: IO ()
 main = do
+    -- TODO - parse ARGV + print help
     text <- readFile "sample.scss"
     compile text >>= print
 
@@ -11,4 +12,4 @@ test :: IO ()
 test = readFile "sample.scss" >>= debugOutput >>= putStrLn
 
 debugOutput :: String -> IO String
-debugOutput c = compile c
+debugOutput = compile
