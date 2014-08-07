f5d284e06b6d04a8cec3addd1ce090db0f32ad5a
Jakub Arnold <darthdeus@gmail.com>
2014-08-07 21:09:57 +0200

    Add more test cases

--- test/Fass/Parser/ColorSpec.hs
+++ test/Fass/Parser/ColorSpec.hs
@@ -14,6 +14,8 @@ spec = do
 
         it "works for rgb" $ do
             testParser colorParser "rgb(255, 255, 0)" `matchRight` RGBA 255 255 0 0.0
+            testParser colorParser "rgb(0, 0, 0)" `matchRight` RGBA 0 0 0 0.0
 
         it "works for rgba" $ do
             testParser colorParser "rgba(255, 255, 0, 3.14)" `matchRight` RGBA 255 255 0 3.14
+            testParser colorParser "rgba(0, 0, 0, 0)" `matchRight` RGBA 0 0 0 0
