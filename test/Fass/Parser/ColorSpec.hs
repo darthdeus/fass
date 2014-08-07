97f2e91c0663da53125bb8619f2b41afa9c33062
Jakub Arnold <darthdeus@gmail.com>
2014-08-07 21:10:21 +0200

    Unify float literals

--- test/Fass/Parser/ColorSpec.hs
+++ test/Fass/Parser/ColorSpec.hs
@@ -9,12 +9,12 @@ spec :: Spec
 spec = do
     describe "colorParser" $ do
         it "works for both short and long hex colors" $ do
-            testParser colorParser "#ffffff" `matchRight` RGBA 255 255 255 0.0
-            testParser colorParser "#fff" `matchRight` RGBA 255 255 255 0.0
+            testParser colorParser "#ffffff" `matchRight` RGBA 255 255 255 0
+            testParser colorParser "#fff" `matchRight` RGBA 255 255 255 0
 
         it "works for rgb" $ do
-            testParser colorParser "rgb(255, 255, 0)" `matchRight` RGBA 255 255 0 0.0
-            testParser colorParser "rgb(0, 0, 0)" `matchRight` RGBA 0 0 0 0.0
+            testParser colorParser "rgb(255, 255, 0)" `matchRight` RGBA 255 255 0 0
+            testParser colorParser "rgb(0, 0, 0)" `matchRight` RGBA 0 0 0 0
 
         it "works for rgba" $ do
             testParser colorParser "rgba(255, 255, 0, 3.14)" `matchRight` RGBA 255 255 0 3.14
