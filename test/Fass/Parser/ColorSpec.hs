eb87a4819be4c65a2ae356b91ed0a9bb15c16b37
Jakub Arnold <darthdeus@gmail.com>
2014-08-07 20:38:34 +0200

    Remove more duplication from specs

--- test/Fass/Parser/ColorSpec.hs
+++ test/Fass/Parser/ColorSpec.hs
@@ -1,19 +1,11 @@
 module Fass.Parser.ColorSpec where
 
 import Fass.Parser.Color
+import Fass.TestHelper
+
 import Test.Hspec
-import Text.Parsec
-import Text.Parsec.String
 
 -- TODO - extract shared logic into something like TestHelper
-testParser :: Parser a -> String -> Either ParseError a
-testParser parser = parse parser "test parser"
-
-matchRight :: (Show a, Show b, Eq b) => Either a b -> b -> IO ()
-matchRight ex y = case ex of
-    Left x -> fail $ show x
-    Right x -> x `shouldBe` y
-
 spec :: Spec
 spec = do
     describe "colorParser" $ do
