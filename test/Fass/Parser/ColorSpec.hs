3e452b841bd5c868671c4e40562c33fbd14b1946
Jakub Arnold <darthdeus@gmail.com>
2014-08-07 20:20:24 +0200

    Add specs for generic color parser

--- test/Fass/Parser/ColorSpec.hs
+++ test/Fass/Parser/ColorSpec.hs
@@ -20,3 +20,10 @@ spec = do
         it "works for both short and long hex colors" $ do
             testParser hexColor "#ffffff" `matchRight` RGB 255 255 255
             testParser hexColor "#fff" `matchRight` RGB 255 255 255
+
+    describe "colorParser" $ do
+        it "works for rgb" $ do
+            testParser colorParser "rgb(255, 255, 0)" `matchRight` RGBA 255 255 0 0.0
+
+        it "works for rgba" $ do
+            testParser colorParser "rgba(255, 255, 0, 3.14)" `matchRight` RGBA 255 255 0 3.14
