1b5f383a03ef948b47b90aa009e6b84ade11e7e0
Jakub Arnold <darthdeus@gmail.com>
2014-08-11 02:28:31 +0200

    Add skeleton for value parser spec

---
+++ test/Fass/Parser/ValueSpec.hs
@@ -0,0 +1,13 @@
+module Fass.Parser.ValueSpec where
+
+import Fass.Parser.Value
+import Fass.TestHelper
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "valueParser" $ do
+        it "works" $ do
+            -- TODO
+            True `shouldBe` True
